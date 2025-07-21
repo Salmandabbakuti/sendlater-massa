d3fc4c82577140df2dfab595565f6560c1d4590d
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2025-07-21 06:28:59 +0000

    added: hello-world boilerplate

---
+++ assembly/contracts/main.ts
@@ -0,0 +1,18 @@
+import { generateEvent, Storage, Context } from '@massalabs/massa-as-sdk';
+
+const GREETING_KEY = 'greeting_key';
+
+/**
+ * This function is meant to be called only one time: when the contract is deployed.
+ */
+export function constructor(_: StaticArray<u8>): void {
+  // This line is important. It ensures that this function can't be called in the future.
+  // If you remove this check, someone could call your constructor function and reset your smart contract.
+  assert(Context.isDeployingContract());
+
+  // Set the greeting message in the contract storage
+  Storage.set(GREETING_KEY, 'Hello, World!');
+
+  // Emit an event to notify that the greeting message has been set
+  generateEvent(`Greeting has been set`);
+}
---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import { bytesToStr, JsonRPCClient } from '@massalabs/massa-web3';
+import { useEffect, useState } from 'react';
+import { MassaLogo } from '@massalabs/react-ui-kit';
+import './App.css';
+
+const massaClient = JsonRPCClient.buildnet();
+const CONTRACT_ADDRESS = 'AS1NJdcsVY7joSCTvMTZnfEPuq1yhw7pNE48Arn1H5gQTsXV5Ss5'; // TODO Update with your deployed contract address
+const GREETING_KEY = 'greeting_key';
+
+export default function App() {
+  const [greeting, setGreeting] = useState(null);
+  useEffect(() => {
+    getGreeting();
+  }, []);
+
+  async function getGreeting() {
+    if (massaClient) {
+      const dataStoreVal = await massaClient.getDatastoreEntry(
+        GREETING_KEY,
+        CONTRACT_ADDRESS,
+        false,
+      );
+      const greetingDecoded = dataStoreVal ? bytesToStr(dataStoreVal) : null;
+      setGreeting(greetingDecoded);
+    }
+  }
+
+  return (
+    <>
+      <MassaLogo className="logo" size={100} />
+      <h2>Greeting message:</h2>
+      <h1>{greeting}</h1>
+    </>
+  );
+}
